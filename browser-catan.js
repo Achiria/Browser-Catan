9fb7ba770ceea595fc34d7534ab89da360475f58
Anthony Gamboa <anthony.b.gamboa@gmail.com>
2020-01-10 22:28:38 -0800

    Added Cities. Toggle selection

---
+++ browser-catan.js
@@ -0,0 +1,33 @@
+var placingRoad = false;
+var placingSettlement = false;
+var placingCity = false;
+
+function placeRoad() {
+    if (!placingRoad) {
+        placingRoad = true;
+        $(".roadSlot").addClass("selectable");
+    } else {
+        placingRoad = false;
+        $(".roadSlot").removeClass("selectable");
+    }
+}
+
+function placeSettlement() {
+    if (!placingSettlement) {
+        placingSettlement = true;
+        $(".settlement").css("display", "block");
+    } else {
+        placingSettlement = false;
+        $(".settlement").css("display", "none");
+    }
+}
+
+function placeCity() {
+    if (!placingCity) {
+        placingCity = true;
+        $(".city").css("display", "block");
+    } else {
+        placingCity = false;
+        $(".city").css("display", "none");
+    }
+}
